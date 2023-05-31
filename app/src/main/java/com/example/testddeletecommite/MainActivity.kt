eae6c4fb720d2f392db44c6d084b2cf607d72c97
Egor <alekseevegorc@gmail.com>
2023-05-31 10:42:23 +0300

    test2

--- app/src/main/java/com/example/testddeletecommite/MainActivity.kt
+++ app/src/main/java/com/example/testddeletecommite/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //check1
+                    //check2
                     Greeting("Android")
                 }
             }
